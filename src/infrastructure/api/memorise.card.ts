ef79f9c84f03790b3fc8b3601c7d3ad17321a456
KJH <bkho@naver.com>
2024-07-05 17:23:20 +0900

    ADD: MemoriseCard 대상으로 임시 api 작성

---
+++ src/infrastructure/api/memorise.card.ts
@@ -0,0 +1,25 @@
+import Http from './http';
+const API_URL = "/resource";
+
+export const MemoriseCardAPI = {
+  async getAll(code:number):Promise<any> {
+    const params = { data: { code } }; 
+    try {
+      const res = await Http.get(API_URL, params);
+  
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  async get(cardNum: number):Promise<any> {
+    const params = { data: { cardNum } }; 
+    try {
+      const res = await Http.get(API_URL, params);
+  
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
