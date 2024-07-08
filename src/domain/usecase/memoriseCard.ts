b5a322de66572fef6baae72bdd4ed499726d0e3a
KJH <bkho@naver.com>
2024-07-08 16:10:51 +0900

    FEAT: 카드 가져온 후 모델화 처리, 에러 처리는 null

--- src/domain/usecase/memoriseCard.ts
+++ src/domain/usecase/memoriseCard.ts
@@ -1,4 +1,6 @@
+import { isEmpty } from "lodash";
 import MemoriseCardRepository from "../../infrastructure/api/memorise.card";
+import MemoriseModel from "../model/memorise_card";
 
 export default class MemoriseCardUsecase {
   private _repository:MemoriseCardRepository;
@@ -14,8 +16,24 @@ export default class MemoriseCardUsecase {
       /**
        * 카드 변환 처리
        */
+      if(!(res instanceof Array) || res.length <= 0) {
+        return null;
+      }
+      const CardList = res.map(item => {
+        return new MemoriseModel({
+          bibleName: item.bible_name,
+          chapter: item.chapter,
+          verse1: item.f_verse,
+          verse2: item.l_verse ?? undefined,
+          content: item.verse_gae,
+          id: item.id,
+        })
+      });
+
+      return CardList;
     } catch (error) {
-      
+      console.error(error);
+      return null;
     }
   }
 
@@ -26,8 +44,22 @@ export default class MemoriseCardUsecase {
       /**
        * 카드 변환 처리
        */
-    } catch (error) {
+      if(isEmpty(res)) return null;
       
+      const Card = new MemoriseModel({
+        bibleName: res.bible_name,
+        chapter: res.chapter,
+        verse1: res.f_verse,
+        verse2: res.l_verse ?? undefined,
+        content: res.verse_gae,
+        id: res.id,
+      });
+      
+      return Card;
+
+    } catch (error) {
+      console.error(error);
+      return null;
     }
   }
 }
