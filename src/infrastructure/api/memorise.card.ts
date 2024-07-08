37504a7b4dcc1cde7e87314eabad32044be1a106
KJH <bkho@naver.com>
2024-07-08 17:20:57 +0900

    FIX: 경로 수정 및 파라미터 수정

--- src/infrastructure/api/memorise.card.ts
+++ src/infrastructure/api/memorise.card.ts
@@ -1,12 +1,12 @@
 import Http from './http';
-const API_URL = "/resource";
+const API_URL = "/RC";
 
 export default class MemoriseCardSeriesRepository  {
 
   async getAll(code:number):Promise<any> {
     const params = { data: { code } }; 
     try {
-      const res = await Http.get(API_URL, params);
+      const res = await Http.get(`${API_URL}/${code}`, params);
   
       return res.data;
     } catch (error) {
